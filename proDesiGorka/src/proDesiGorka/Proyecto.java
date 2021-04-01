a97c4175f303e500f742003ce6546c8a81579258
Gorka <Gorka@DESKTOP-A8VQGSK>
2021-04-01 22:37:02 +0200

    Añado un if

--- proDesiGorka/src/proDesiGorka/Proyecto.java
+++ proDesiGorka/src/proDesiGorka/Proyecto.java
@@ -4,10 +4,13 @@ public class Proyecto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int x=0;
 		System.out.println("Hola Desi");
 		System.out.println("Que tal?");
 		System.out.println("Yo bien y tu?");
+		if(x==0) {
+			System.out.println("Funciona");
+		}
 	}
 
 }
