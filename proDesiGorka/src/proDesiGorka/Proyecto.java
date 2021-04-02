7d4a9d7d47f4ecf51c40685940356226ad41b3c5
Gorka <Gorka@DESKTOP-A8VQGSK>
2021-04-02 15:15:44 +0200

    Trabajando juntos

--- proDesiGorka/src/proDesiGorka/Proyecto.java
+++ proDesiGorka/src/proDesiGorka/Proyecto.java
@@ -17,7 +17,9 @@ int x=0;
 		}
 		
 		System.out.println("Verificar si se realiza el cambio");
-		system.out.println("Cambio en el remoto.")
+		System.out.println("Cambio en el remoto.");
+		
+		System.out.println("Ya estamos trabajando juntos");
 
 }
 }
