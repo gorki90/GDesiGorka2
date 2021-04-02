b0f672dfbb27667d62d3048aecc8b32cc0a88cc6
Gorka <Gorka@DESKTOP-A8VQGSK>
2021-04-02 18:30:40 +0200

    Modificacion de la nueva rama

--- proDesiGorka/src/proDesiGorka2/Rama.java
+++ proDesiGorka/src/proDesiGorka2/Rama.java
@@ -4,6 +4,7 @@ public class Rama {
 	
 	void subir() {
 		System.out.println("Subimos la nueva rama");
+		System.out.println("Hago una modificacion");
 	}
 	
 	public static void main(String[] args) {
