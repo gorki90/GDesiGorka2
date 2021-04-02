5b8243f7a8a273c2b2535c8044687ec4847b17eb
Gorka <Gorka@DESKTOP-A8VQGSK>
2021-04-02 18:28:59 +0200

    Nueva clase y paquete.

---
+++ proDesiGorka/src/proDesiGorka2/Rama.java
@@ -0,0 +1,14 @@
+package proDesiGorka2;
+
+public class Rama {
+	
+	void subir() {
+		System.out.println("Subimos la nueva rama");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
